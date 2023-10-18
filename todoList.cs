c482202b1e3e750bb8260298b3659c14de7beb4f
zdisanto <70993217+zdisanto@users.noreply.github.com>
2023-10-18 15:57:52 -0400

    Update todoList.cs

--- todoList.cs
+++ todoList.cs
@@ -5,34 +5,35 @@ public class Program {
 	public static void Main() {
 		// To make the todo list:
 		List<string> todoList = new List<string>();
+		string number = "";
 		
 		// To grab current date & time:
 		DateTime date = DateTime.Now;
 		
 		// Todo List Header:
 		Console.WriteLine("== Welcome to your Terminal Todo List ==");
-       	Console.WriteLine("Today is {0:d} at {0:T} UTC.\n", date);
+       		Console.WriteLine("Today is {0:d} at {0:T} UTC.\n", date);
 		
 		// Todo List Functionality:
-		while (true) {
+		while (number != "4") {
 			Console.WriteLine("Type a number and press ENTER to complete a function:");
 			Console.WriteLine("\t1. Add Task");
 			Console.WriteLine("\t2. Delete Task");
 			Console.WriteLine("\t3. List Tasks");
 			Console.WriteLine("\t4. Exit Program");
 			// Grabbing the number the user chose (can't user userChoice...)
-			int number = int.Parse(Console.ReadLine());
+			number = Console.ReadLine();
 			
 			// If user presses '1': 
-			if (number == 1) {
+			if (number == "1") {
 				Console.Write("Add a task: "); 
 			 	string task = Console.ReadLine(); 
 				todoList.Add(task); 
-			 	Console.Clear(); 
-			 	Console.WriteLine("Your task was added successfully!");
+				Console.Clear(); 
+			 	Console.WriteLine("Your task was added successfully!\n");
 				
 			// If user presses '2': 
-			} else if (number == 2) {
+			} else if (number == "2") {
 				if (todoList.Count > 0) { 
  					Console.WriteLine("Enter the number of the task you want to delete:");
 					for (int i = 0; i < todoList.Count; i++) { 
@@ -53,11 +54,13 @@ public class Program {
 					}
 				// If there are no items in the todo list:
 				} else { 
+					Console.Clear(); 
 					Console.WriteLine("There are no items in your Todo list."); 
 					Console.WriteLine("");
 				} 
 			// If user presses '3': 
-			} else if (number == 3) {
+			} else if (number == "3") {
+				Console.Clear();
 				// If there are no items in the todo list:
 				if (todoList.Count > 0) { 
 					Console.WriteLine("Your Todo List: ");
@@ -65,18 +68,21 @@ public class Program {
 						Console.WriteLine("\t- " + todoList[i]); 
 					}
 				} else {
+					Console.Clear();
 					Console.WriteLine("There are no items in your Todo list."); 
 					Console.WriteLine("");
 				}
 				Console.WriteLine("");
 				
 			// If user presses '4':
-			} else if (number == 4) { 
- 			  break;
+			} else if (number == "4") { 
+ 				break;
 			// If user presses anything other than 1-4, error statement:
 			} else { 
 				Console.WriteLine("404: Request Not Found.\n"); 
 			}
-		} Console.WriteLine("Good bye!"); 
+		} 
+		Console.Clear();
+		Console.WriteLine("Good bye!"); 
 	}
 }
